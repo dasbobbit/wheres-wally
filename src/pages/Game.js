6bf0c9b75e9b5a52b2895772c57210f0a9b80b86
dasbobbit <bcrossley95@gmail.com>
2021-05-12 22:06:36 +0100

    Added image

---
+++ src/pages/Game.js
@@ -0,0 +1,24 @@
+import React from "react"
+import imageBattle from "../images/wally-battle.jpg"
+
+const Game = () => {
+
+    const handleClick = (e) => {
+        console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        console.log(e);
+        if (e.nativeEvent.offsetX >= 1387 && e.nativeEvent.offsetX <= 1422 
+            && e.nativeEvent.offsetY >= 465 && e.nativeEvent.offsetY <= 501) {
+                console.log("clicked wallo")
+        } else {
+            console.log("NOT clicked wallo")
+        }
+    }
+
+    return (
+        <div>
+            <img onClick={handleClick} src={imageBattle} alt="waldly" />
+        </div>
+    )
+}
+
+export default Game
